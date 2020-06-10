61e527bab432c7a6a59a5ffa0a31f7dfcbb2174e
Suman Kumar <sumancsnit@gmail.com>
2020-06-10 21:44:02 +0530

    Updates

---
+++ precache-manifest.fb1a2d160055da6e03e5e3a6d5b5c682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64e72dc2515001767466552548ab4e8",
+    "url": "/react-cattitude-cards/index.html"
+  },
+  {
+    "revision": "096199e9a51720a88779",
+    "url": "/react-cattitude-cards/static/css/main.96f1b793.chunk.css"
+  },
+  {
+    "revision": "cfd90c875049bc95c3b3",
+    "url": "/react-cattitude-cards/static/js/2.9e9dd4a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cattitude-cards/static/js/2.9e9dd4a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096199e9a51720a88779",
+    "url": "/react-cattitude-cards/static/js/main.ecd2aac6.chunk.js"
+  },
+  {
+    "revision": "d387ad95afea44b19d37",
+    "url": "/react-cattitude-cards/static/js/runtime-main.64827bc1.js"
+  }
+]);
